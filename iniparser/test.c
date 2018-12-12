a5b63b1766c64ad09b671824cc25c6933835f13b
chenkuanglu <chenkuanglu@qq.com>
2018-12-12 13:44:45 +0800

    concat sec & key

--- iniparser/test.c
+++ iniparser/test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "iniparser.h"
 
+#define CFG_MAKE_KEY(sec, key)      CFG_CONCAT_STR(sec, key)
+#define CFG_CONCAT_STR(sec, key)    sec##_##key
+
+#define __CFG_STRING(x)             (#x)
+#define CFG_STRING(x)               __CFG_STRING(x)
+#define CFG_PROGRAM_ADDR            CFG_STRING(CFG_MAKE_KEY(program, address))
+
 int main(void)
 {
     dictionary *dict = NULL;
@@ -10,11 +17,13 @@ int main(void)
         return -1;
     }
 
-    unsigned addr = iniparser_getlongint(dict, "program:program_addr", -1);
+    unsigned addr = iniparser_getlongint(dict, "program:address", -1);
     printf("get program addr = 0x%08x\n", addr);
 
     const char* str = iniparser_getstring(dict, "nonce:nonce_hit", "none");
     printf("get nonce_hit str = '%s'\n", str);
+
+    printf("concat = '%s'\n", CFG_PROGRAM_ADDR);
 
     iniparser_freedict(dict);
     return 0;

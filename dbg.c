dc6818c2a7f1fe2aeeef406b3860a3699b185068
chenkuanglu <chenkuanglu@qq.com>
2018-12-27 23:48:29 +0800

    parse ini & arg

--- dbg.c
+++ dbg.c
@@ -1,17 +1,70 @@
 // debugger 
 
-#include "xconfig.h"
 #include <curses.h>
+#include "thr_queue.h"
+#include "xconfig.h"
+#include "argparser.h"
 
 // type of main control block
 typedef struct {
     char*           inifile;    // *.ini file
     xconfig_t*      config;     // config info
+    argparser_t*    cmdline;    // cmdline arguments
+    int             run_mode;
 } dbg_cb_t;
 
-int dbg_run(dbg_cb_t *dbg)
+enum {
+    RUN_MODE_NORMAL,    // normal 
+    RUN_MODE_HELP,      // print help info, '-h x' or 'x --help'
+    RUN_MODE_VERSION,   // print version
+};
+
+static int cmdline_proc(long id, char **param, int num)
+{
+    (void)num;
+
+    switch (id) {
+    case 'h':
+        printfd("print help info...\n");
+        break;
+    case 'v':
+        printfd("print version info...\n");
+        break;
+    case 'd':
+        printfd("set serial device: %s\n", param[0]);
+        break;
+    case 1001:  // --baud
+        printfd("set baud: %s\n", param[0]);
+        break;
+    default:
+        printfd(CCL_YELLOW "unknown arg id %d\n" CCL_END, id);
+        break;
+    }
+
+    return 0;
+}
+
+static int dbg_run(dbg_cb_t *dbg)
 {
     (void)dbg;
+
+    thrq_cb_t *myq = thrq_create(NULL);
+    if (myq == NULL) {
+        printfd(CCL_RED "Fail to create queue\n" CCL_END);
+    }
+    
+    double tmout = 2;
+    char *buf[32];
+    for (;;) {
+        int ret = thrq_receive(myq, &buf, sizeof(buf), tmout);
+        if (ret == ETIMEDOUT) {
+            printfd(CCL_YELLOW "%.2fs timeout...\n" CCL_END, tmout);
+        } else {
+            printfd(CCL_RED "error occured: %s\n" CCL_END, strerror(errno));
+        }
+    }
+
+    return 0;
 }
 
 int main(int argc, char **argv)
@@ -26,8 +79,9 @@ int main(int argc, char **argv)
         return -1;
     }
     dbg->inifile = strdup("dbg.ini");
+    dbg->run_mode = RUN_MODE_NORMAL;
 
-    // Open & parser ini file
+    // Open & parse ini file
     dbg->config = xconfig_new();
     if (dbg->config == NULL) {
         printfd(CCL_RED "Fail to new 'xconfig_t'\n" CCL_END);
@@ -40,28 +94,46 @@ int main(int argc, char **argv)
         return -1;
     }
     
+    // Parse command line 
+    dbg->cmdline = argparser_new(argc, argv);
+    if (dbg->cmdline == NULL) {
+        printfd(CCL_RED "Fail to new 'argparser_t'\n" CCL_END);
+    }
+    argparser_add(dbg->cmdline, "-h", 'h', 0);
+    argparser_add(dbg->cmdline, "--help", 'h', 0);
+    argparser_add(dbg->cmdline, "-v", 'v', 0);
+    argparser_add(dbg->cmdline, "-d", 'd', 1);
+    argparser_add(dbg->cmdline, "--baud", 1001, 1);
+    if (argparser_parse(dbg->cmdline, cmdline_proc)) {
+        printfd(CCL_RED "Fail to process command line\n" CCL_END);
+    }
+
+    // Run the debugger 
     dbg_run(dbg);
 
-    initscr();
-    WINDOW *mywin = newwin(10, 10, 2, 2);
-    //box(mywin, ACS_VLINE, ACS_HLINE);
-    box(mywin, 0, 0);
-    //move(LINES/2 + 1, COLS/2 - sizeof("hello, world!")/2);
-    move(4, 4);
-    waddstr(mywin, "hello, world!");
-
-    hline(0, 20);
-    vline(0, 20);
-    wrefresh(mywin);
-    wrefresh(stdscr);
-    getch();
-
-    wclear(mywin);
-    delwin(mywin);
-    endwin();
-    
+    // Release resources
+    free(dbg->inifile);
     xconfig_delete(dbg->config);
+    argparser_delete(dbg->cmdline);
 
     return 0;
 }
 
+
+//initscr();
+//WINDOW *mywin = newwin(10, 10, 2, 2);
+////box(mywin, ACS_VLINE, ACS_HLINE);
+//box(mywin, 0, 0);
+////move(LINES/2 + 1, COLS/2 - sizeof("hello, world!")/2);
+//move(4, 4);
+//waddstr(mywin, "hello, world!");
+
+//hline(0, 20);
+//vline(0, 20);
+//wrefresh(mywin);
+//wrefresh(stdscr);
+//getch();
+
+//wclear(mywin);
+//delwin(mywin);
+//endwin();

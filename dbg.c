9119513b346c25aebed29f977d6c82e6ac136a04
chenkuanglu <chenkuanglu@qq.com>
2019-02-19 13:47:24 +0800

    -

--- dbg.c
+++ dbg.c
@@ -4,7 +4,7 @@
 #include <curses.h>
 #endif
 #include <signal.h>
-#include "thr_queue.h"
+#include "thrq.h"
 #include "xconfig.h"
 #include "argparser.h"
 
@@ -202,7 +202,7 @@ static int dbg_run(void)
 
     logn("Serial Debugger Version %s\n", DBG_VERSION);
 
-    thrq_cb_t *myq = thrq_create(NULL);
+    thrq_cb_t *myq = thrq_new(NULL);
     if (myq == NULL) {
         loge("Fail to create queue\n");
     }

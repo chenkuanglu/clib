e980e37f762c9ee0bceefb89fd416c04807415fe
chenkuanglu <chenkuanglu@qq.com>
2019-03-21 20:58:57 +0800

    ---

--- build_dbg.sh
+++ build_dbg.sh
@@ -1,5 +1,5 @@
 # debug
-gcc -o dbg.out dbg.c iniparser/xconfig.c iniparser/dictionary.c iniparser/iniparser.c argparser/argparser.c -DDEBUG -I./utils -I./iniparser -I./argparser -L./utils -lcurses -lutils -lpthread -lm
+gcc -o dbg-debug.out dbg.c iniparser/xconfig.c iniparser/dictionary.c iniparser/iniparser.c argparser/argparser.c -DDEBUG -I./utils -I./iniparser -I./argparser -L./utils -lcurses -lutils -lpthread -lm
 
 # release
-#gcc -o dbg.out dbg.c iniparser/xconfig.c iniparser/dictionary.c iniparser/iniparser.c argparser/argparser.c -I./utils -I./iniparser -I./argparser -L./utils -lcurses -lutils -lpthread -lm
+gcc -o dbg.out dbg.c iniparser/xconfig.c iniparser/dictionary.c iniparser/iniparser.c argparser/argparser.c -I./utils -I./iniparser -I./argparser -L./utils -lcurses -lutils -lpthread -lm
